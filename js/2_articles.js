ff38230612504046a2270c6f6471d8f4229c5e15
William <w.abrego20@gmail.com>
2024-10-12 11:26:18 -0500

    actualizandocasa25

---
+++ js/2_articles.js
@@ -0,0 +1,128 @@
+/*
+ *Función para visualizar la tabla de los articulos
+ */
+$(document).ready(function () {
+  $("#tableArticles").DataTable({
+    language: {
+      processing: "Procesando...",
+      lengthMenu: "Mostrar _MENU_ articulos",
+      zeroRecords: "No se encontraron articulos",
+      emptyTable: "Ningún articulo disponible en esta tabla",
+      info: "Mostrando articulo del _START_ al _END_ de un total de _TOTAL_ articulos",
+      infoEmpty: "Mostrando articulo del 0 al 0 de un total de 0 articulos",
+      infoFiltered: "(filtrado de un total de _MAX_ articulos)",
+      search: "Buscar:",
+      loadingRecords: "Cargando...",
+      paginate: {
+        first: "Primero",
+        last: "Último",
+        next: "Siguiente",
+        previous: "Anterior",
+      },
+      aria: {
+        sortAscending: ": Activar para ordenar la columna de manera ascendente",
+        sortDescending: ": Activar para ordenar la columna de manera descendente",
+      },
+    },
+    dom: "lBfrtip", // 'l' es para el selector de longitud
+    buttons: [
+      {
+        text: '<i class="fa-solid fa-heart-circle-plus"></i> Crear articulo',
+        action: function (e, dt, node, config) {
+          var formu = document.querySelector(".modalCreateArticle"); //mostrar el modal de crear artículo
+          formu.style.display = "flex";
+          setTimeout(function () {
+            formu.classList.add("show");
+          }, 10);
+        },
+      },
+    ],
+  });
+});
+/*
+ *Función para ocultar el modal de crear artículo
+ */
+function ocultarFormCreateArticle() {
+  var modal = document.querySelector(".modalCreateArticle");
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+  setTimeout(function () {
+    modal.style.display = "none";
+    modal.classList.remove("hide");
+  }, 500);
+}
+/*
+ *Función para cargar foto de articulo
+ */
+function btnArticlesPhoto() {
+  document.getElementById("btnArticlesPhoto").click();
+}
+document
+  .getElementById("btnArticlesPhoto")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        document.querySelector(
+          ".btnArticlesPhoto .fa-camera-retro"
+        ).style.display = "none";
+        const preview = document.getElementById("articles_photo");
+        preview.src = e.target.result;
+        preview.style.display = "flex";
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Función para ponerle el signo $ al campo costo unitario y redondear a dos decimales
+ * @param {*} input - es un número
+ */
+/* function removeNonNumeric(input) {
+  // Elimina cualquier carácter que no sea un número o un punto decimal
+  input.value = input.value.replace(/[^0-9.]/g, "");
+}
+function formatCurrency(input) {
+  let value = parseFloat(input.value).toFixed(2);
+  if (isNaN(value)) {
+    value = "0.00";
+  }
+  input.value = `$${value}`;
+}
+ 
+
+
+
+
+
+
+
+
+
+/**
+ * Deshabilitar dias posteriores a la actual para
+ */
+/* document.addEventListener('DOMContentLoaded', (event) => {
+  const dateInput = document.getElementById('articles_arrival_date');
+  const today = new Date().toISOString().split('T')[0];
+  dateInput.setAttribute('max', today);
+}); */
